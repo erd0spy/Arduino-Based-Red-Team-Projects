cb1bb0b25eecf4c89a1c5544994bcb3f93060681
Fikret Garipay <58850695+erd0spy@users.noreply.github.com>
2022-01-23 14:59:40 +0300

    Update arduino_rubber_duckySD.ino

--- Arduino-Rubber-Ducky/arduino_rubber_duckySD.ino
+++ Arduino-Rubber-Ducky/arduino_rubber_duckySD.ino
@@ -9,7 +9,7 @@ String SCRIPT = "script.txt";
 
 void setup(){
 
-  if (!SD.begin(4)) {
+  if (!SD.begin(4)){
     return;
   }
   
@@ -23,16 +23,16 @@ void setup(){
       if (m == '\n'){
         Line(line);
         line = "";
-        }
-        else if((int) m != 13){
-          line += m;
-        }
+      }
+      else if((int) m != 13){
+        line += m;
+      }
     }
     Line(line);
     
     scriptFile.close();
-  } else{
-  }
+  } 
+  else{}
 
   Keyboard.end();
 }
@@ -51,20 +51,20 @@ void Line(String l){
   }
   else if(l.substring(0,space_1) == "REM"){}
   else{
-      String remain = l;
+    String remain = l;
     
-      while(remain.length() > 0){
-        int latest_space = remain.indexOf(" ");
-        if (latest_space == -1){
-          Press(remain);
-          remain = "";
-        }
-        else{
-          Press(remain.substring(0, latest_space));
-          remain = remain.substring(latest_space + 1);
-        }
-        delay(5);
+    while(remain.length() > 0){
+      int latest_space = remain.indexOf(" ");
+      if (latest_space == -1){
+        Press(remain);
+        remain = "";
       }
+      else{
+        Press(remain.substring(0, latest_space));
+        remain = remain.substring(latest_space + 1);
+      }
+      delay(5);
+    }
   }
 
   Keyboard.releaseAll();
@@ -80,95 +80,94 @@ void Press(String b){
   }
   else if (b.equals("CTRL")){
     Keyboard.press(KEY_LEFT_CTRL);
-  }
-    else if (b.equals("SHIFT")){
+    }
+  else if (b.equals("SHIFT")){
     Keyboard.press(KEY_LEFT_SHIFT);
   }
-    else if (b.equals("ALT")){
+  else if (b.equals("ALT")){
     Keyboard.press(KEY_LEFT_ALT);
   }
-    else if (b.equals("GUI")){
+  else if (b.equals("GUI")){
     Keyboard.press(KEY_LEFT_GUI);
   }
-    else if (b.equals("UP") || b.equals("UPARROW")){
+  else if (b.equals("UP") || b.equals("UPARROW")){
     Keyboard.press(KEY_UP_ARROW);
   }
-    else if (b.equals("DOWN") || b.equals("DOWNARROW")){
+  else if (b.equals("DOWN") || b.equals("DOWNARROW")){
     Keyboard.press(KEY_DOWN_ARROW);
   }
-    else if (b.equals("LEFT") || b.equals("LEFTARROW")){
+  else if (b.equals("LEFT") || b.equals("LEFTARROW")){
     Keyboard.press(KEY_LEFT_ARROW);
   }
-    else if (b.equals("RIGHT") || b.equals("RIGHTARROW")){
+  else if (b.equals("RIGHT") || b.equals("RIGHTARROW")){
     Keyboard.press(KEY_RIGHT_ARROW);
   }
-    else if (b.equals("DELETE")){
+  else if (b.equals("DELETE")){
     Keyboard.press(KEY_DELETE);
   }
-    else if (b.equals("PAGEUP")){
+  else if (b.equals("PAGEUP")){
     Keyboard.press(KEY_PAGE_UP);
   }
-    else if (b.equals("PAGEDOWN")){
+  else if (b.equals("PAGEDOWN")){
     Keyboard.press(KEY_PAGE_DOWN);
   }
-    else if (b.equals("HOME")){
+  else if (b.equals("HOME")){
     Keyboard.press(KEY_HOME);
   }
-    else if (b.equals("ESC")){
+  else if (b.equals("ESC")){
     Keyboard.press(KEY_ESC);
   }
-    else if (b.equals("INSERT")){
+  else if (b.equals("INSERT")){
     Keyboard.press(KEY_INSERT);
   }
-    else if (b.equals("TAB")){
+  else if (b.equals("TAB")){
     Keyboard.press(KEY_TAB);
   }
-    else if (b.equals("END")){
+  else if (b.equals("END")){
     Keyboard.press(KEY_END);
   }
-    else if (b.equals("CAPSLOCK")){
+  else if (b.equals("CAPSLOCK")){
     Keyboard.press(KEY_CAPS_LOCK);
   }
-    else if (b.equals("F1")){
+  else if (b.equals("F1")){
     Keyboard.press(KEY_F1);
   }
-    else if (b.equals("F2")){
+  else if (b.equals("F2")){
     Keyboard.press(KEY_F2);
   }
-    else if (b.equals("F3")){
+  else if (b.equals("F3")){
     Keyboard.press(KEY_F3);
   }
-    else if (b.equals("F4")){
+  else if (b.equals("F4")){
     Keyboard.press(KEY_F4);
   }
-    else if (b.equals("F5")){
+  else if (b.equals("F5")){
     Keyboard.press(KEY_F5);
   }
-    else if (b.equals("F6")){
+  else if (b.equals("F6")){
     Keyboard.press(KEY_F6);
   }
-    else if (b.equals("F7")){
+  else if (b.equals("F7")){
     Keyboard.press(KEY_F7);
   }
-    else if (b.equals("F8")){
+  else if (b.equals("F8")){
     Keyboard.press(KEY_F8);
   }
-    else if (b.equals("F9")){
+  else if (b.equals("F9")){
     Keyboard.press(KEY_F9);
   }
-    else if (b.equals("F10")){
+  else if (b.equals("F10")){
     Keyboard.press(KEY_F10);
   }
-    else if (b.equals("F11")){
+  else if (b.equals("F11")){
     Keyboard.press(KEY_F11);
   }
-    else if (b.equals("F12")){
+  else if (b.equals("F12")){
     Keyboard.press(KEY_F12);
   }
-    else if (b.equals("SPACE")){
+  else if (b.equals("SPACE")){
     Keyboard.press(' ');
   } 
 }
 
-void loop(){
-}
+void loop(){}
